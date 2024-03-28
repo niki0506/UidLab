15d54fd4be526bfbbaf0b9d02da1674cf341293b
niki0506 <153723521+niki0506@users.noreply.github.com>
2024-03-28 10:37:42 +0530

    report_college 28/03

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'collegeDataSet.student' table. You can move, or remove it, as needed.
+        Me.studentTableAdapter.Fill(Me.collegeDataSet.student)
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+End Class
